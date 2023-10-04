e4d9679373ef5694f45287e051878dcf09cfda60
vadimkalaida <vadosgta1@gmail.com>
2023-10-04 20:44:45 +0300

    parse.service - code looks ugly, but everything seems to work

--- server/src/global-services/parse.service.ts
+++ server/src/global-services/parse.service.ts
@@ -7,11 +7,11 @@ interface IStringWithNumberOfTabs {
 
 @Injectable()
 export class ParseService {
-	prepareData(stringsWithTabs: IStringWithNumberOfTabs[], tabs: number = 0, customI: number = 0) {
-		const res = {};
+	prepareData(stringsWithTabs: IStringWithNumberOfTabs[], tabs: number = 0, customInitialIterationIndex: number = 0) {
+		const res: Record<string, any> = {};
 		let stringsWithTabsCopy = [...stringsWithTabs];
 		let foundSmallerTab = false;
-		let i = customI || 0;
+		let i = customInitialIterationIndex || 0;
 		while (!foundSmallerTab && i < stringsWithTabsCopy.length) {
 			const currentString = stringsWithTabsCopy[i];
 			console.log(currentString, "currentString", i);
@@ -37,18 +37,6 @@ export class ParseService {
 					console.log("prevstirng", prevString);
 					res[prevString.value] = [];
 				}
-				// if (
-				// 	(prevString &&
-				// 		!prevString.value.includes("=") &&
-				// 		!currentString.value.includes("=") &&
-				// 		prevString.tabs > currentString.tabs && !res[prevString.value]) ||
-				// 	(nextString &&
-				// 		!currentString.value.includes("=") &&
-				// 		!nextString.value.includes("=") &&
-				// 		nextString.tabs > currentString.tabs)
-				// ) {
-				// 	res[currentString.value] = [];
-				// }
 				if (
 					prevString &&
 					Array.isArray(res[prevString.value]) &&
@@ -57,9 +45,6 @@ export class ParseService {
 					!currentString.value.includes("=")
 				) {
 					console.log(currentString, "currentString obj");
-					// res[prevString.value].push({
-					// 	[nextString.value.split("=")[0].trim()]: nextString.value.split("=")[1].trim(),
-					// });
 					const foundNextItemWithThisTab = stringsWithTabsCopy
 						.slice(i + 1)
 						.find((item) => item.tabs === currentString.tabs);
@@ -67,7 +52,7 @@ export class ParseService {
 					const foundIndex = foundNextItemWithThisTab ? stringsWithTabsCopy.indexOf(foundNextItemWithThisTab) : -1;
 					res[prevString.value].push({
 						...this.prepareData(
-							stringsWithTabsCopy.slice(i || 0, foundIndex >= -1 ? foundIndex : stringsWithTabsCopy.length - 1),
+							stringsWithTabsCopy.slice(i || 0, foundIndex >= 0 ? foundIndex : stringsWithTabsCopy.length),
 							currentString.tabs,
 							1
 						),
@@ -83,14 +68,12 @@ export class ParseService {
 							...stringsWithTabsCopy.slice(foundIndex > 0 ? foundIndex - 1 : stringsWithTabsCopy.length),
 						];
 					}
-					// i = foundIndex > 0 ? foundIndex - 1 : stringsWithTabsCopy.length - 1;
 					i = 0;
 					console.log(i, "i", "foundIndex", foundIndex);
 				}
-				if (currentString.value.includes("=") && ((prevString && prevString.value.includes("=")) || !prevString)) {
+				if (currentString.value.includes("=")) {
 					res[currentString.value.split("=")[0].trim()] = currentString.value.split("=")[1].trim();
 				}
-				// this.prepareData(stringsWithTabs, currentString.tabs);
 			} else {
 				console.log(currentString, "currentString");
 				foundSmallerTab = true;
@@ -100,15 +83,6 @@ export class ParseService {
 			}
 		}
 		return res;
-		// for (let i = 0; i < stringsWithTabs.length && stringsWithTabs[i].tabs > tabs; i += 1) {
-		// 	if (
-		// 		!stringsWithTabs[i].value.includes("=") &&
-		// 		stringsWithTabs[i + 1] &&
-		// 		!stringsWithTabs[i + 1].value.includes("=")
-		// 	) {
-		// 		res[stringsWithTabs[i].value] = [];
-		// 	}
-		// }
 	}
 
 	parse(content: string) {
@@ -122,8 +96,6 @@ export class ParseService {
 		});
 		const prepared = this.prepareData(stringsWithNumberOfTabs);
 		console.log(prepared);
-		console.log(prepared["exchange-offices"], "prepared exchange-offices");
-		console.log(prepared["exchange-offices"][0].exchanges, "prepared exchange-offices[0].exchanges");
 		return stringsWithNumberOfTabs;
 	}
 }
