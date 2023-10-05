bd6e6af764ad2dfbbfb00b50b6327de306c1f98a
vadimkalaida <vadosgta1@gmail.com>
2023-10-05 17:39:21 +0300

    remmoved unnecessary files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { FileModule } from "./file/file.module";
 
 @Module({
 	imports: [FileModule],
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
