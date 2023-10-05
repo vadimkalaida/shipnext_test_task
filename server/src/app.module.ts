c785f288fbd8ce2f466117ebe2874a549c631bfc
vadimkalaida <vadosgta1@gmail.com>
2023-10-05 20:23:34 +0300

    db config initialization

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { FileModule } from "./file/file.module";
 
 @Module({
-	imports: [FileModule],
+	imports: [
+		FileModule,
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: `.env.${process.env.NODE_ENV}`,
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				type: "postgres",
+				host: configService.get("DB_HOST"),
+				port: configService.get("DB_PORT"),
+				username: configService.get("DB_USERNAME"),
+				password: configService.get("DB_PASSWORD"),
+				database: configService.get("DB_DATABASE"),
+				synchronize: true,
+				entities: [__dirname, "/**/*.entity{.ts,.js}"],
+			}),
+			inject: [ConfigService],
+		}),
+	],
 	controllers: [],
 	providers: [],
 })
